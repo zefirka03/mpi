f13f3503277e0bccfce37dce260449301ddd8974
zefirka03 <newpontos@gmail.com>
2024-05-16 03:22:20 +0300

    Sending from parent to childs

---
+++ src/Application.h
@@ -0,0 +1,226 @@
+#pragma once
+#include "core/AirCore.h"
+
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+
+#include <mpi.h>
+
+#include "GridTexture.h"
+#include "Solution2.h"
+
+
+constexpr int WIDTH = 1280;
+constexpr int HEIGHT = 1280;
+
+
+struct DeviceVertexInstance {
+    glm::vec2 position;
+    glm::vec2 texCoord;
+};
+
+
+class Lab1_Solver : public Solver {
+public:
+    double solve(int cell_index, std::vector<Cell> const& cells, glm::vec2 a, double deltaTime) override {
+        double bound_u = 0.f;
+
+        auto& cell = cells[cell_index];
+
+        int c_up_i = cell.faces[Face::FaceDirection::up].neib_index;
+        int c_down_i = cell.faces[Face::FaceDirection::down].neib_index;
+        int c_left_i = cell.faces[Face::FaceDirection::left].neib_index;
+        int c_right_i = cell.faces[Face::FaceDirection::right].neib_index;
+
+        double c_up_u = c_up_i != CELL_NULL ? cells[c_up_i].u : bound_u;
+        double c_down_u = c_down_i != CELL_NULL ? cells[c_down_i].u : bound_u;
+        double c_left_u = c_left_i != CELL_NULL ? cells[c_left_i].u : bound_u;
+        double c_right_u = c_right_i != CELL_NULL ? cells[c_right_i].u : bound_u;
+
+        double x_comp = a.x >= 0.f ? cell.u - c_left_u : c_right_u - cell.u;
+        double y_comp = a.y >= 0.f ? cell.u - c_down_u : c_up_u - cell.u;
+
+        return cell.u - deltaTime * ((a.x * x_comp / cell.size.x) + (a.y * y_comp / cell.size.y));
+    }
+};
+
+class Lab2_Solver : public Solver {
+public:
+    double solve(int cell_index, std::vector<Cell> const& cells, glm::vec2 a, double deltaTime) override {
+        double bound_u = 0.f;
+
+        auto& cell = cells[cell_index];
+
+        int c_up_i = cell.faces[Face::FaceDirection::up].neib_index;
+        int c_down_i = cell.faces[Face::FaceDirection::down].neib_index;
+        int c_left_i = cell.faces[Face::FaceDirection::left].neib_index;
+        int c_right_i = cell.faces[Face::FaceDirection::right].neib_index;
+
+        double c_up_u = c_up_i != CELL_NULL ? cells[c_up_i].u : bound_u;
+        double c_down_u = c_down_i != CELL_NULL ? cells[c_down_i].u : bound_u;
+        double c_left_u = c_left_i != CELL_NULL ? cells[c_left_i].u : bound_u;
+        double c_right_u = c_right_i != CELL_NULL ? cells[c_right_i].u : bound_u;
+
+        int c_ul_i = c_up_i != CELL_NULL ? cells[c_up_i].faces[Face::FaceDirection::left].neib_index : CELL_NULL;
+        int c_ur_i = c_up_i != CELL_NULL ? cells[c_up_i].faces[Face::FaceDirection::right].neib_index : CELL_NULL;
+        int c_dl_i = c_down_i != CELL_NULL ? cells[c_down_i].faces[Face::FaceDirection::left].neib_index : CELL_NULL;
+        int c_dr_i = c_down_i != CELL_NULL ? cells[c_down_i].faces[Face::FaceDirection::right].neib_index : CELL_NULL;
+
+        double c_ul_u = c_ul_i != CELL_NULL ? cells[c_ul_i].u : bound_u;
+        double c_ur_u = c_ur_i != CELL_NULL ? cells[c_ur_i].u : bound_u;
+        double c_dl_u = c_dl_i != CELL_NULL ? cells[c_dl_i].u : bound_u;
+        double c_dr_u = c_dr_i != CELL_NULL ? cells[c_dr_i].u : bound_u;
+
+        return cell.u -
+            deltaTime * a.x * (c_right_u - c_left_u) / (2.0 * cell.size.x) -
+            deltaTime * a.y * (c_up_u - c_down_u) / (2.0 * cell.size.y) +
+            deltaTime * deltaTime * a.x * a.x * (c_left_u - 2.0 * cell.u + c_right_u) / (2.0 * cell.size.x * cell.size.x) +
+            deltaTime * deltaTime * a.y * a.y * (c_down_u - 2.0 * cell.u + c_up_u) / (2.0 * cell.size.y * cell.size.y) +
+            deltaTime * deltaTime * (a.x * a.y + a.y * a.x) * (c_ur_u - c_ul_u - c_dr_u + c_dl_u) / (8.0 * cell.size.y * cell.size.x);
+
+    }
+};
+
+/*
+class Lab3_Solver : public Solver {
+public:
+    double solve(int cell_index, std::vector<Cell> const& cells, glm::vec2 a, double deltaTime) override {
+        Lab1_Solver solve_up;
+        Lab2_Solver solve_lax;
+
+        double F_up = solve_up.solve(cell_index, cells, a, deltaTime);
+        double F_lax = solve_lax.solve(cell_index, cells, a, deltaTime);
+
+        auto phi = [](double r) -> bool {
+
+        };
+
+        return F_up + phi(cells[cell_index] - ) * (F_lax - F_up);
+    }
+};
+*/
+
+
+class Application {
+public:
+    static int run_child(int rank) {
+        int32_t buffer_size;
+        MPI_Recv(&buffer_size, 1, MPI_INT32_T, 0, SLVR_MPI_TAG_LOCALS_SEND_DATA_COUNT, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+        std::vector<std::byte> locals_buffer(buffer_size);
+        MPI_Recv(locals_buffer.data(), buffer_size, MPI_BYTE, 0, SLVR_MPI_TAG_LOCALS_SEND_DATA, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+        Mesh mesh;
+        mesh.unpack_locals(locals_buffer);
+
+        return 0;
+    }
+
+    static int run_parent() {
+        if (!glfwInit())
+            return 1;
+
+        GLFWwindow* m_window = glfwCreateWindow(WIDTH, HEIGHT, "lab", NULL, NULL);
+        if (!m_window) {
+            glfwTerminate();
+            return 1;
+        }
+
+        glfwMakeContextCurrent(m_window);
+
+        glewInit();
+        glfwSwapInterval(1);
+
+        air::Shader shader;
+        shader.load_from_string(
+            std::string({ R"(~~vertex~~
+#version 430 core
+layout(location = 0) in vec2 position;
+layout(location = 1) in vec2 texCoord;
+
+out vec2 o_texCoord;
+
+void main() {
+	gl_Position = vec4(position, 0.0, 1.0); 
+    o_texCoord = texCoord;
+}
+ 
+~~fragment~~
+#version 430 core
+
+out vec4 out_color;
+in vec2 o_texCoord;
+
+uniform sampler2D uTexture;
+
+void main() {
+	out_color = texture(uTexture, o_texCoord);
+}
+)" }).c_str(),
+AIR_SHADER_VF);
+
+        std::vector<DeviceVertexInstance> vert_data = {
+            DeviceVertexInstance{glm::vec2(-1.0, -1.0), glm::vec2(0.0, 0.0)},
+            DeviceVertexInstance{glm::vec2(+1.0, -1.0), glm::vec2(1.0, 0.0)},
+            DeviceVertexInstance{glm::vec2(+1.0, +1.0), glm::vec2(1.0, 1.0)},
+            DeviceVertexInstance{glm::vec2(-1.0, -1.0), glm::vec2(0.0, 0.0)},
+            DeviceVertexInstance{glm::vec2(+1.0, +1.0), glm::vec2(1.0, 1.0)},
+            DeviceVertexInstance{glm::vec2(-1.0, +1.0), glm::vec2(0.0, 1.0)},
+        };
+
+        air::VAO vao;
+        vao.pushVBO(0, 0, 2, sizeof(DeviceVertexInstance), 0 * sizeof(GLfloat)).buffer(vert_data.data(), sizeof(DeviceVertexInstance) * vert_data.size());
+        vao.pushVBO(0, 1, 2, sizeof(DeviceVertexInstance), 2 * sizeof(GLfloat));
+
+        int sz = 512;
+        GridTexture grid(sz, sz);
+
+        Solution2 sol(512, 512.f, 512, 512,
+            new Lab2_Solver(),
+            [](double x, double y) -> double {
+                float sz = 512 * 0.5;
+                return
+                    std::abs(x - sz) < 50 &&
+                    std::abs(y - sz) < 50 ? 1 : 0.;
+            },
+            [](double x, double y) {
+                //float sz = 512 * 0.5;
+                //if (x != sz || y != sz)
+                //    return glm::normalize(glm::vec2(x - sz, y - sz));
+                //return glm::vec2(x / sz, y / sz);
+                return glm::vec2(1);
+            });
+
+        Mesh mesh(512.f, 512.f, 512, 512,
+            [](double x, double y) -> double {
+                float sz = 512 * 0.5;
+                return
+                    std::abs(x - sz) < 50 &&
+                    std::abs(y - sz) < 50 ? 1 : 0.;
+            }
+        );
+        mesh.redistribute();
+        mesh.apply_to_texture_rank_map(grid);
+
+        glClearColor(0.2, 0.2, 0.2, 0.2);
+        while (!glfwWindowShouldClose(m_window)) {
+            glClear(GL_COLOR_BUFFER_BIT);
+            if (glfwGetKey(m_window, GLFW_KEY_SPACE) == GLFW_PRESS) {
+                sol.iterate(0.25f);
+                sol.apply_to_texture(grid);
+            }
+
+            grid.bind_texture();
+            shader.use();
+            vao.bind();
+            glDrawArrays(GL_TRIANGLES, 0, vert_data.size());
+            vao.unbind();
+
+            glfwSwapBuffers(m_window);
+            glfwPollEvents();
+        }
+
+        return 0;
+	}
+};
--- src/Solution2.h
+++ src/Solution2.h
@@ -1,11 +1,16 @@
 #pragma once
 #include <array>
 #include <algorithm>
+#include <unordered_map>
+#include <mpi.h>
 
 #include "core/AirCore.h"
 #include "GridTexture.h"
 
 #define CELL_NULL -1
+
+#define SLVR_MPI_TAG_LOCALS_SEND_DATA_COUNT 1
+#define SLVR_MPI_TAG_LOCALS_SEND_DATA 2
 
 
 struct Face {
@@ -16,12 +21,22 @@ struct Face {
         down = 3
     };
 
+    // индекс соседа в его массиве locals
+    // (то есть если сосед удаленный, то index показывает
+    // номер сосдней ячейки в массиве locals на том процессе)
     int neib_index = CELL_NULL;
+    // индекс соседа в массиве aliens (если сосед удаленный)
+    int alien = CELL_NULL;
+    // ранг соседа
+    int rank;
 };
 
 
 struct Cell {
+    // номер в массиве locals
     int index = CELL_NULL;
+    // ранг ячейки
+    int rank; 
 
     double u = 0;
     double u_back = 0;
@@ -37,6 +52,153 @@ struct Cell {
 class Solver {
 public:
     virtual double solve(int cell_index, std::vector<Cell> const& cells, glm::vec2 a, double deltaTime) = 0;
+};
+
+
+class Mesh {
+private:
+    // Локальные ячейки
+    std::vector<Cell> m_locals;
+    // Ячейки с других процессов
+    std::vector<Cell> m_aliens; 
+
+    glm::vec2 m_grid_size;
+    // Size of grid in world space
+
+    int _get_new_rank(Cell const& cell){
+        return 1 + int(cell.center.x / (m_grid_size.x / 2.f)) + 2 * int(cell.center.y / (m_grid_size.y / 2.f));
+    }
+
+public:
+    Mesh() {}
+
+    Mesh(
+        double size_x,
+        double size_y,
+        int cells_x,
+        int cells_y,
+        double (*u0_function)(double, double)
+    ) {
+        initialize_grid(
+            size_x,
+            size_y,
+            cells_x,
+            cells_y,
+            u0_function
+        );
+    }
+
+    void initialize_grid(
+        double size_x,
+        double size_y,
+        int cells_x,
+        int cells_y,
+        double (*u0_function)(double, double)
+    ) {
+        m_grid_size = glm::vec2(size_x, size_y);
+
+        double cell_size_x = size_x / cells_x;
+        double cell_size_y = size_y / cells_y;
+
+        std::vector<std::vector<Cell>> temp_cells(cells_x, std::vector<Cell>(cells_y, Cell()));
+
+        // Init cells base
+        for (int y = 0; y < cells_y; ++y) {
+            for (int x = 0; x < cells_x; ++x) {
+                temp_cells[x][y].index = y * size_y + x;
+
+                temp_cells[x][y].size = glm::vec2(cell_size_x, cell_size_y);
+                temp_cells[x][y].position = glm::vec2(cell_size_x * x, cell_size_y * y);
+                temp_cells[x][y].center = temp_cells[x][y].position + temp_cells[x][y].size / 2.f;
+
+                temp_cells[x][y].u = u0_function(temp_cells[x][y].center.x, temp_cells[x][y].center.y);
+            }
+        }
+
+        // Init cells faces
+        for (int y = 0; y < cells_y; ++y) {
+            for (int x = 0; x < cells_x; ++x) {
+                int up = y + 1 < cells_y ? temp_cells[x][y + 1].index : CELL_NULL;
+                int down = y - 1 >= 0 ? temp_cells[x][y - 1].index : CELL_NULL;
+                int right = x + 1 < cells_x ? temp_cells[x + 1][y].index : CELL_NULL;
+                int left = x - 1 >= 0 ? temp_cells[x - 1][y].index : CELL_NULL;
+
+                temp_cells[x][y].faces[Face::FaceDirection::up].neib_index = up;
+                temp_cells[x][y].faces[Face::FaceDirection::down].neib_index = down;
+                temp_cells[x][y].faces[Face::FaceDirection::right].neib_index = right;
+                temp_cells[x][y].faces[Face::FaceDirection::left].neib_index = left;
+            }
+        }
+
+        // Copy
+        m_locals = std::vector<Cell>(cells_x * cells_y, Cell());
+        for (int i = 0; i < cells_y; ++i)
+            std::copy(
+                temp_cells[i].begin(),
+                temp_cells[i].end(),
+                m_locals.data() + cells_x * i
+            );
+    }
+
+    void redistribute() {
+        std::unordered_map<int, std::vector<Cell>> rank_locals;
+
+        // just apply rank for every cell
+        for(int i = 0; i < m_locals.size(); ++i){
+            m_locals[i].rank = _get_new_rank(m_locals[i]);
+            rank_locals[m_locals[i].rank].push_back(m_locals[i]);
+        }
+
+        // send locals data to every process
+        for (auto rank_it : rank_locals) {
+            int32_t buffer_size = rank_it.second.size() * sizeof(Cell);
+            MPI_Send(&buffer_size, 1, MPI_INT32_T, rank_it.first, SLVR_MPI_TAG_LOCALS_SEND_DATA_COUNT, MPI_COMM_WORLD);
+            MPI_Send(rank_it.second.data(), rank_it.second.size() * sizeof(Cell), MPI_BYTE, rank_it.first, SLVR_MPI_TAG_LOCALS_SEND_DATA, MPI_COMM_WORLD);
+        }
+    }
+
+    void exchange() {
+        for (int i = 0; i < m_locals.size(); ++i) {
+            
+        }
+    }
+
+    void apply_to_texture_rank_map(GridTexture& texture){
+        for(int i = 0; i < m_locals.size(); ++i){
+            glm::vec2 relate_pos = m_locals[i].center / m_grid_size;
+
+            int x_p = int(relate_pos.x * texture.size().x);
+            int y_p = int(relate_pos.y * texture.size().y);
+
+            double color = std::max(0.0, std::min(255.0, (float)(m_locals[i].rank) * 255.0 * 0.25));
+            texture.set_pixel(x_p, y_p, {
+                    uint8_t(color),
+                    uint8_t(color),
+                    uint8_t(color)
+                }
+            );
+        }
+        
+        texture.update();
+    }
+
+    std::vector<std::byte> pack_locals() {
+        std::vector<std::byte> pocket(m_locals.size() * sizeof(Cell));
+        std::memcpy(pocket.data(), m_locals.data(), m_locals.size() * sizeof(Cell));
+        return pocket;
+    }
+
+    static std::vector<std::byte> pack_locals(std::vector<Cell> const& locals) {
+        std::vector<std::byte> pocket(locals.size() * sizeof(Cell));
+        std::memcpy(pocket.data(), locals.data(), locals.size() * sizeof(Cell));
+        return pocket;
+    }
+
+    void unpack_locals(std::vector<std::byte> const& pocket_data) {
+        m_locals.resize(pocket_data.size() * sizeof(std::byte) / sizeof(Cell));
+        std::memcpy(m_locals.data(), pocket_data.data(), pocket_data.size() * sizeof(std::byte));
+    }
+
 };
 
 
